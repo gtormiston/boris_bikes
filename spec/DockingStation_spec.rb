52ec7aaf801c98c782baa678e257e1a9ac424ac8
Graham Ormiston <grahamormiston@gmail.com>
2016-05-25 17:42:42 +0100

    complex attributes complete

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,7 +4,7 @@ require 'Bike'
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
  	it { is_expected.to respond_to(:dock).with(1).argument }
-	it { is_expected.to respond_to(:bike)}
+	it { is_expected.to respond_to(:bikes)}
 
 	describe '#release_bike' do
 		it 'raises an error when there are no bikes available' do
@@ -14,7 +14,7 @@ describe DockingStation do
 
 	describe '#dock' do
 		it 'raises an error if the docking station is full' do
-			subject.dock(Bike.new)
+			20.times { subject.dock(Bike.new) }
 			expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
 		end
 	end
