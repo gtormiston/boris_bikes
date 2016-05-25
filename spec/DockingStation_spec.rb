8c50a163b842a4e6a251632d27c74e59bda3b393
a-mellor <alexander.mellor@gmail.com>
2016-05-25 16:29:20 +0100

    Limiting capacity

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,8 +7,15 @@ describe DockingStation do
 	it { is_expected.to respond_to(:bike)}
 
 	describe '#release_bike' do
-		it 'releases a bike' do
+		it 'raises an error when there are no bikes available' do
 			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
+
+	describe '#dock' do
+		it 'raises an error if the docking station is full' do
+			subject.dock(Bike.new)
+			expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
 		end
 	end
 end
